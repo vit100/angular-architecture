a7e32c0622d02eaeb4714065a169a152bab0bc1f
Vitaliy Markitanov <9357021+vit100@users.noreply.github.com>
2019-04-17 10:57:24 -0400

    Update app-routing.module.ts
    These 2 imports are not in use

--- demos/src/app/app-routing.module.ts
+++ demos/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PlanningComponent } from './planning/planning.component';
-import { FeaturesModulesComponent } from './features-modules/features-modules.component';
-
 const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo: '/planning' },
   { path: 'features-modules', loadChildren: 'app/features-modules/features-modules.module#FeaturesModulesModule' }
